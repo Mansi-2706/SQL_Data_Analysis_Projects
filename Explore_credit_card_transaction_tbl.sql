d2832af746a66578b7aed0480bb1ebfba7bb37a4
Mansi-2706 <131741818+Mansi-2706@users.noreply.github.com>
2023-08-09 17:55:36 +0530

    Add files via upload
    This is the brief description about the dataset taken form Kaggle, link: https://www.kaggle.com/datasets/thedevastator/analyzing-credit-card-spending-habits-in-india
    Imported the dataset in SQL Server with table name : credit_card_transactions
    Changed the column names to lower case before importing data to SQL Server. Also replaced space within column names with underscore.

---
+++ Explore_credit_card_transaction_tbl.sql
@@ -0,0 +1,45 @@
+SELECT * FROM [dbo].[credit_card_transactions$];
+
+--checking the range of data
+SELECT MIN(transaction_date), MAX(transaction_date) FROM [dbo].[credit_card_transactions$];
+--[2013-10-04 00:00:00.000
+--2015-05-26 00:00:00.000]
+
+--checking different card types
+SELECT DISTINCT card_type FROM [dbo].[credit_card_transactions$]
+/*
+card_type
+Silver
+Signature
+Gold
+Platinum
+*/
+
+--checking different expense types
+SELECT DISTINCT exp_type FROM [dbo].[credit_card_transactions$]
+/*
+exp_type
+Entertainment
+Food
+Bills
+Fuel
+Grocery
+*/
+
+SELECT DISTINCT gender FROM [dbo].[credit_card_transactions$]
+/*
+GENDER
+F
+M
+*/
+
+SELECT DISTINCT COUNT(*) FROM [dbo].[credit_card_transactions$]
+--16383
+
+
+
+
+
+
+
+
